10a0263766621574b014d9f3ba36a55f5aae9fd0
Abhijay Sahay <abhijaygame@gmail.com>
2019-11-26 13:33:35 +0000

    Fixed text tag list

--- latexwc.c
+++ latexwc.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 
 #include "latexwc.h"
+#include "tag_array.h"
 
 bool getContents(const char* filename, char** contents, long* length) {
     FILE* file = fopen(filename, "r");
@@ -20,4 +21,50 @@ bool getContents(const char* filename, char** contents, long* length) {
 
     (*contents)[*length] = '\0';
     return true;
+}
+
+tagArray* getTextTags() {
+    char* contents;
+    long length;
+
+    if(!getContents(CONFIG_FILE_PATH, &contents, &length)) {
+        return NULL;
+    }
+    
+    char c;
+    char tag[20];
+    // char* tagToAppend;
+    int lastTagLength = 0;
+    tagArray* textTagList = newTagArray(APPROX_TEXT_TAG_COUNT);
+
+    for(long i = 0, end = length + 1; i < end; i++) {
+        c = contents[i];
+        
+        switch(c) {
+            case ' ':
+                // ignore
+                break;
+            case '\n':
+            case '\0':
+                // add to array
+                if(lastTagLength != 0) {
+                    tag[lastTagLength] = '\0';
+
+                    // more memory efficient way to do this?
+                    // will this work?
+                    // sprintf(tagToAppend, "%s", tag);
+                    tagArrayAppendAllocateElement(textTagList, tag);
+                }
+                lastTagLength = 0;
+                break;
+            default:
+                tag[lastTagLength] = c;
+                lastTagLength += 1;
+                break;
+        }
+    }
+
+    free(contents);
+    return textTagList;
+    // add last word, if the file wasn't empty
 }
